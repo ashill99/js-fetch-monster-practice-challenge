daf04f79c3f4e84c9cf9db198361abbd76253cb3
ashill99 <admshng@gmail.com>
2020-12-19 18:23:00 -0500

    commit tree e885626bad8fb6ceada0074ff5d735a9b5def855

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const url = "http://localhost:3000/monsters"
+const urlOne = "http://localhost:3000/monsters?_limit=50&_page=1"
+const urlTwo = "http://localhost:3000/monsters?_limit=50&_page=2"
+//***** DOM Element *******
+
+//*****EVENT Handlers*******
+
+//******Render Functions******
+
+
+//*****Fetch Functions *******
+
+
+//******Initial Render ******
+
+// When the page loads, show the first 50 monsters. Each monster's name, age, and description should be shown.
